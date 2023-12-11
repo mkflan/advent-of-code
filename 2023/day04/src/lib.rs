6da8a54dedfff50cccfccbe944b9e12d4f474cdb
mkflan <makani.flan@gmail.com>
2023-12-11 17:17:19 -0600

    day 4 complete

--- 2023/day04/src/lib.rs
+++ 2023/day04/src/lib.rs
@@ -1,11 +1,90 @@
-#![allow(unused)]
+use nom::{
+    bytes::complete::take_until,
+    character::complete::{self, line_ending, space1},
+    multi::{fold_many1, separated_list1},
+    sequence::{separated_pair, terminated},
+    IResult, Parser,
+};
+use nom_supreme::{tag::complete::tag, ParserExt};
+use std::collections::{HashMap, HashSet};
 
-pub fn solve_part1(input: &str) -> usize {
-    0
+#[derive(Debug)]
+struct Card {
+    winning_numbers: HashSet<u32>,
+    our_numbers: HashSet<u32>,
 }
 
-pub fn solve_part2(input: &str) -> usize {
-    0
+impl Card {
+    fn winners(&self) -> u32 {
+        self.winning_numbers.intersection(&self.our_numbers).count() as u32
+    }
+
+    fn card_score(self) -> u32 {
+        self.winners()
+            .checked_sub(1)
+            .map(|p| 2u32.pow(p))
+            .unwrap_or(0)
+    }
+}
+
+fn card_side(side: &str) -> IResult<&str, HashSet<u32>> {
+    space1
+        .precedes(fold_many1(
+            terminated(complete::u32, space1),
+            HashSet::new,
+            |mut acc, num| {
+                acc.insert(num);
+                acc
+            },
+        ))
+        .parse(side)
+}
+
+fn card(card: &str) -> IResult<&str, Card> {
+    take_until(":")
+        .precedes(tag(":"))
+        .precedes(separated_pair(card_side, tag("|"), card_side))
+        .map(|(winning_numbers, our_numbers)| Card {
+            winning_numbers,
+            our_numbers,
+        })
+        .parse(card)
+}
+
+fn cards(input: &str) -> IResult<&str, Vec<Card>> {
+    separated_list1(line_ending, card).parse(input)
+}
+
+fn parse_input(input: &str) -> Vec<Card> {
+    cards(input).unwrap().1
+}
+
+pub fn solve_part1(input: &str) -> u32 {
+    parse_input(input).into_iter().map(Card::card_score).sum()
+}
+
+pub fn solve_part2(input: &str) -> u32 {
+    let cards = parse_input(input);
+
+    let card_to_count = (0..cards.len())
+        .map(|card| (card, 1))
+        .collect::<HashMap<usize, u32>>();
+
+    cards
+        .into_iter()
+        .enumerate()
+        .fold(card_to_count, |mut ctc, (card_id, card)| {
+            let to_add = *ctc.get(&card_id).unwrap();
+            let winners = card.winners();
+
+            for next_card_id in card_id + 1..=card_id + winners as usize {
+                ctc.entry(next_card_id).and_modify(|c| *c += to_add);
+            }
+
+            ctc
+        })
+        .values()
+        .sum()
 }
 
 #[cfg(test)]
