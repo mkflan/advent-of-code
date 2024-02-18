1e8aa6c7e54da3e23a9ed5de97114779487bf829
mkflan <makani.flan@gmail.com>
2024-02-18 10:29:49 -0600

    update

--- 2023/day07/src/lib.rs
+++ 2023/day07/src/lib.rs
@@ -1,7 +1,129 @@
 #![allow(unused)]
 
-pub fn solve_part1(input: &str) -> usize {
-    0
+use itertools::Itertools;
+use nom::{
+    bytes::complete::is_a,
+    character::complete::{self, line_ending, space1},
+    multi::separated_list1,
+    sequence::separated_pair,
+    IResult, Parser,
+};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Card {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+impl Card {
+    fn from_char(ch: char) -> Self {
+        use Card::*;
+
+        match ch {
+            '2' => Two,
+            '3' => Three,
+            '4' => Four,
+            '5' => Five,
+            '6' => Six,
+            '7' => Seven,
+            '8' => Eight,
+            '9' => Nine,
+            'T' => Ten,
+            'J' => Jack,
+            'Q' => Queen,
+            'K' => King,
+            'A' => Ace,
+            _ => panic!("Invalid card."),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum HandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+#[derive(Debug, Clone)]
+struct Hand {
+    cards: Vec<Card>,
+    bid: u32,
+}
+
+impl Hand {
+    fn hand_type(&self) -> HandType {
+        use HandType::*;
+
+        let counts = self.cards.iter().counts();
+        let mut counts = counts.values();
+
+        if counts.len() == 1 {
+            FiveOfAKind
+        } else if counts.len() == 2 {
+            if counts.any(|c| *c == 4) {
+                FourOfAKind
+            } else {
+                FullHouse
+            }
+        } else if counts.len() == 3 {
+            if counts.any(|c| *c == 3) {
+                ThreeOfAKind
+            } else {
+                TwoPair
+            }
+        } else if counts.len() == 4 {
+            OnePair
+        } else {
+            HighCard
+        }
+    }
+}
+
+fn cards(cards: &str) -> IResult<&str, Vec<Card>> {
+    is_a("23456789TJQKA")
+        .map(|s: &str| s.chars().map(Card::from_char).collect())
+        .parse(cards)
+}
+
+fn hand(hand: &str) -> IResult<&str, Hand> {
+    separated_pair(cards, space1, complete::u32)
+        .map(|(cards, bid)| Hand { cards, bid })
+        .parse(hand)
+}
+
+fn parse_hands(hands: &str) -> IResult<&str, Vec<Hand>> {
+    separated_list1(line_ending, hand).parse(hands)
+}
+
+fn parse_input(input: &str) -> Vec<Hand> {
+    parse_hands(input).unwrap().1
+}
+
+pub fn solve_part1(input: &str) -> u32 {
+    let hands = parse_input(input);
+
+    hands
+        .into_iter()
+        .sorted_by_key(|h| (h.hand_type(), h.cards.clone()))
+        .enumerate()
+        .map(|(idx, hand)| hand.bid * (idx + 1) as u32)
+        .sum()
 }
 
 pub fn solve_part2(input: &str) -> usize {
--- 2023/day12/src/lib.rs
+++ 2023/day12/src/lib.rs
@@ -1,6 +1,55 @@
 #![allow(unused)]
 
+use nom::{
+    bytes::complete::is_a,
+    character::complete::{self, line_ending, space1},
+    multi::separated_list1,
+    sequence::separated_pair,
+    IResult, Parser,
+};
+use nom_supreme::tag::complete::tag;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum SpringCondition {
+    Unknown,
+    Damaged,
+    Operational,
+}
+
+impl SpringCondition {
+    fn from_char(ch: char) -> Self {
+        use SpringCondition::*;
+
+        match ch {
+            '?' => Unknown,
+            '#' => Damaged,
+            '.' => Operational,
+            _ => panic!("Invalid spring condition"),
+        }
+    }
+}
+
+fn springs(spring: &str) -> IResult<&str, Vec<SpringCondition>> {
+    is_a("?#.")
+        .map(|s: &str| s.chars().map(SpringCondition::from_char).collect())
+        .parse(spring)
+}
+
+fn record(record: &str) -> IResult<&str, (Vec<SpringCondition>, Vec<u32>)> {
+    separated_pair(springs, space1, separated_list1(tag(","), complete::u32)).parse(record)
+}
+
+fn parse_records(records: &str) -> IResult<&str, Vec<(Vec<SpringCondition>, Vec<u32>)>> {
+    separated_list1(line_ending, record).parse(records)
+}
+
+fn parse_input(input: &str) -> Vec<(Vec<SpringCondition>, Vec<u32>)> {
+    parse_records(input).unwrap().1
+}
+
 pub fn solve_part1(input: &str) -> usize {
+    let records = parse_input(input);
+
     0
 }
 
@@ -14,9 +63,14 @@ mod tests {
 
     #[test]
     fn test_with_sample() {
-        let sample_input = "";
+        let sample_input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
 
-        assert_eq!(0, solve_part1(sample_input));
+        assert_eq!(21, solve_part1(sample_input));
         assert_eq!(0, solve_part2(sample_input));
     }
 }
